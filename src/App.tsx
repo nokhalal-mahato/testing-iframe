c9f0d5f6ed729162e37df09cad5fa897904f3674
nokhalal.mahato <nokhalal.mahato@nxtwave.co.in>
2025-11-07 17:50:30 +0530

    added code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <iframe
+        src="https://prompt2ui.earlywave.in/"
+        allow="cross-origin-isolated"
+        style={{ width: "100%", height: "100%" }}
+      />
+    </div>
+  );
+}
+
+export default App;
